d1edca7c5f418309a716046b952843cdfbc46275
shahriar808 <shasan5525@gmail.com>
2024-07-12 19:51:38 +0600

    392. Is Subsequence

---
+++ src/IsSubsequence.java
@@ -0,0 +1,40 @@
+public class IsSubsequence {
+    public Boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        int n = t.length();
+        int m = s.length();
+        char ss[] = s.toCharArray();
+        char tt[] = t.toCharArray();
+
+        if (m < 1)
+            return true;
+
+        while (i < n) {
+            if (tt[i] == ss[j]) {
+                j++;
+            }
+            i++;
+            if (j == m)
+                return true;
+        }
+        return false;
+
+       /* if (s.isEmpty())    return true;
+        int i = 0, j = 0;
+        while (i < t.length() && j < s.length()) {
+            if (t.charAt(i) ==s.charAt(j)) {
+                j++;
+            }
+            i++;
+            if (j == s.length()) {
+                return true;
+            }
+        }
+        return false;*/
+    }
+
+    public static void main(String[] args) {
+        IsSubsequence isSubsequence = new IsSubsequence();
+        System.out.println(isSubsequence.isSubsequence("abc", "ahbgdc"));
+    }
+}
