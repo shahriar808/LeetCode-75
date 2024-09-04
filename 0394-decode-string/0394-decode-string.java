861c1812a58509ba2295844819f176041c39d214
Shahriar Hasan <73174988+shahriar808@users.noreply.github.com>
2024-09-04 20:00:27 +0600

    Time: 0 ms (100%), Space: 41.4 MB (61.21%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,37 +1,28 @@
 class Solution {
     public String decodeString(String s) {
-      Stack<Integer> countStack = new Stack<>();
-      Stack<String> resultStack = new Stack<>();
-      String res = "";
-      int index = 0;
-      while (index < s.length()) {
-          if (Character.isDigit(s.charAt(index))) {
-              int count = 0;
-              while (Character.isDigit(s.charAt(index))) {
-                  count = count * 10 + (s.charAt(index) - '0');
-                  index++;
-              }
-              countStack.push(count);
-          } else if (s.charAt(index) == '[') {
-              resultStack.push(res);
-              res = "";
-              index++;
-          }
-          else if (s.charAt(index) == ']') {
-              StringBuilder sb = new StringBuilder(resultStack.pop());
-              int count = countStack.pop();
-              while (count > 0) {
-                  sb.append(res);
-                  count--;
-              }
-              res = sb.toString();
-              index++;
-          }
-          else {
-              res = res + s.charAt(index);
-              index++;
-          }
-      }
-      return res;
+     Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stackSB = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        int n = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                n = n * 10 + c - '0';
+            } else if (c == '[') {
+                countStack.push(n);
+                n = 0;
+                stackSB.push(res);
+                res = new StringBuilder();
+            } else if (c == ']') {
+                int n1 = countStack.pop();
+                StringBuilder sb = res;
+                res = stackSB.pop();
+                for (int i = 0; i < n1; i++) {
+                    res.append(sb);
+                }
+            }else {
+                res.append(c);
+            }
+        }
+        return res.toString();
     }
 }
